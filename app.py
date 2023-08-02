638724b9267ad64108a37a4cf0890a65246df354
Erik Kaunismäki <erik.kaum@gmail.com>
2023-08-02 18:52:44 +0300

    small changes

--- app.py
+++ app.py
@@ -39,10 +39,10 @@ def handler(context: dict, request: Request) -> Response:
         prompt=prompt,
         negative_prompt=negative_prompt,
         guidance_scale=7,
-        num_inference_steps=request.json.get("steps", 50),
+        num_inference_steps=request.json.get("steps", 30),
         generator=torch.Generator(device="cuda").manual_seed(request.json.get("seed")) if request.json.get("seed") else None,
         width=512,
-        height=768,
+        height=512,
     ).images[0]
 
     buffered = BytesIO()
